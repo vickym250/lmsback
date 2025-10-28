873ae2642cf82a6a3fcfb283e11a28f54f3eeee8
Vikram Maurya <162531239+vickym250@users.noreply.github.com>
2025-10-28 13:07:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ try {
 }
 
 // Connect to MongoDB
-const MONGO_URI ="mongodb+srv://vtech250m_db_user:OjGWnzoY6iT3cQP7@cluster0.uctpl7d.mongodb.net/"
+const MONGO_URI ="mongodb+srv://vtech250m_db_user:OjGWnzoY6iT3cQP7@cluster0.uctpl7d.mongodb.net/lms_db";
+
 mongoose.connect(MONGO_URI,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
